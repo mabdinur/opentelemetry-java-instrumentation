2908f4f801b3014e9f62a5727c1c92183f562519
Munir Abdinur <munir_abdinur@hotmail.com>
2020-06-14 12:54:51 -0400

    store changes

---
+++ instrumentation-core/spring/src/main/java/io/opentelemetry/instrumentation/spring/annotations/Config.java
@@ -0,0 +1,25 @@
+package io.opentelemetry.instrumentation.spring.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Traced specifies the annotated method should be included in the Trace.
+ *
+ * @since 0.16.0
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Config {
+
+  /**
+   * The optional custom span name.
+   *
+   * @return the optional custom span name; if not specified the method name will be used as the
+   *     span name
+   */
+  String name() default "";
+  boolean includingLoggingExporter() default true;
+}
---
+++ instrumentation-core/spring/src/main/java/io/opentelemetry/instrumentation/spring/aop/OTaspect.java
@@ -0,0 +1,68 @@
+package io.opentelemetry.instrumentation.spring.aop;
+
+import java.lang.reflect.Method;
+import java.util.logging.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import io.opentelemetry.instrumentation.spring.annotations.TracedClass;
+import io.opentelemetry.instrumentation.spring.annotations.TracedMethod;
+import io.opentelemetry.trace.Tracer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Aspect
+public class OTaspect {
+
+  public static final Logger LOG = Logger.getLogger(OTaspect.class.getName());
+
+  @Autowired
+  private Tracer tracer;
+
+  @Around("@annotation(io.opentelemetry.instrumentation.spring.annotations.TracedMethod)")
+  public Object tracedMethod(final ProceedingJoinPoint pjp) throws Throwable {
+
+    MethodSignature signature = (MethodSignature) pjp.getSignature();
+    Method method = signature.getMethod();
+
+    TracedMethod annotation = method.getAnnotation(TracedMethod.class);
+    if (annotation == null) {
+      return pjp.proceed();
+    }
+
+    String spanName = annotation.name();
+    if (spanName.isEmpty()) {
+      spanName = method.getName();
+    }
+    LOG.info("Traced Span " + spanName);
+    return Handler.proceed(pjp, tracer, spanName, annotation.isEvent());
+  }
+
+  @Around("@annotation(io.opentelemetry.instrumentation.spring.annotations.TracedClass)")
+  public Object tracedMethod(final ProceedingJoinPoint pjp, final TracedClass tracedClass)
+      throws Throwable {
+
+    if (tracedClass == null) {
+      return pjp.proceed();
+    }
+
+    String methodName = getMethodName(pjp, tracedClass.name());
+
+    LOG.info("Traced Span Class " + methodName);
+
+    return Handler.proceed(pjp, tracer, methodName, tracedClass.isEvent());
+
+  }
+
+  private String getMethodName(final ProceedingJoinPoint pjp, String methodName) {
+    if (methodName.isEmpty()) {
+      MethodSignature signature = (MethodSignature) pjp.getSignature();
+      Method method = signature.getMethod();
+      methodName = method.getName();
+    }
+    return methodName;
+  }
+
+}
