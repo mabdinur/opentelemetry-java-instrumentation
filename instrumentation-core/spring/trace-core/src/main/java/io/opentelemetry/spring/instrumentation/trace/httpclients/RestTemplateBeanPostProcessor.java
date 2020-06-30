28ed89f8ea7ca9d9793e637329e0491675b07b0f
Munir Abdinur <munir_abdinur@hotmail.com>
2020-06-30 17:02:37 -0400

    trace-core implementation with web handlers

---
+++ instrumentation-core/spring/trace-core/src/main/java/io/opentelemetry/spring/instrumentation/trace/httpclients/RestTemplateBeanPostProcessor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.opentelemetry.spring.instrumentation.trace.httpclients;
+
+import io.opentelemetry.trace.Tracer;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * BeanProcessor Implementation inspired by: <br>
+ *
+ * @see <a href=
+ *      "https://github.com/spring-cloud/spring-cloud-sleuth/blob/master/spring-cloud-sleuth-core/src/main/java/org/springframework/cloud/sleuth/instrument/web/client/TraceWebClientAutoConfiguration.java">
+ *      spring-cloud-sleuth-core </a>
+ */
+public final class RestTemplateBeanPostProcessor implements BeanPostProcessor {
+
+  private final Tracer tracer;
+
+  public RestTemplateBeanPostProcessor(Tracer tracer) {
+    this.tracer = tracer;
+  }
+
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String beanName) {
+    if (bean instanceof RestTemplate) {
+      RestTemplate restTemplate = (RestTemplate) bean;
+      restTemplate.getInterceptors().add(new RestTemplateInterceptor(tracer));
+      return restTemplate;
+    }
+    return bean;
+  }
+}
